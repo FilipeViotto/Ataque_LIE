5cdf81a729f2563111f9b3118a43ac71724c399f
Filipe Viotto <filipe.filipe2009@gmail.com>
2025-08-20 23:03:43 -0300

    fix: ataque corrigirdo erro na concexão entre ataque e atualização dos modelos

--- defesas.py
+++
@@ -1,9 +0,0 @@
-import args
-args = args.Arguments()
-from modelo import MLP
-
-def TrimmedMean(modelos: list):
-    modelo_resultante = MLP()
-    lista_parametros_aberta = []
-    for modelo in modelos:
-        lista_parametros_aberta = modelo
